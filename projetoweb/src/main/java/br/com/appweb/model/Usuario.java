375c27a6c682aaeafc7e52ea98323a2ad967b57b
RafaMari <RafaMari@Pedro-PC>
2017-03-16 21:59:48 -0300

    Inserido as paginas index.xhtml

---
+++ projetoweb/src/main/java/br/com/appweb/model/Usuario.java
@@ -0,0 +1,27 @@
+package br.com.appweb.model;
+
+public class Usuario {
+	
+	private String nome;
+	private String senha;
+	
+	
+	
+	// Generet and Getters & Setters
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+
+}
